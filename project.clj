a2c22181f8a87cb2b003781e8d845bfa04305d5f
gered <gered@blarg.ca>
2014-03-25 11:08:59 -0400

    first version!

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-metasearch "0.1.0-SNAPSHOT"
+(defproject clj-metasearch "0.1"
   :description "Helper functions for searching through Clojure namespaces for vars containing specific bits of metadata."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/gered/clj-metasearch"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
